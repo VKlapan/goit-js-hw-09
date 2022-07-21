28b9217ac2f1ca7e8f572808e134d7ce9cbbe22d
VKlapan <vladimir.klapan@gmail.com>
2022-07-21 17:51:45 +0300

    add task#01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const bodyEl = document.querySelector('body');
 
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
+
+btnStopEl.setAttribute('disabled', '');
+
+//let isActive = false;
+let intervalId = null;
+
+const changeBodyColor = () =>
+  bodyEl.setAttribute('style', `background-color : ${getRandomHexColor()}`);
+
+const startSwitcher = () => {
+  //  isActive = true;
+  btnStartEl.setAttribute('disabled', '');
+  btnStopEl.removeAttribute('disabled');
+  intervalId = setInterval(changeBodyColor, 1000);
+};
+
+const stopSwitcher = () => {
+  clearInterval(intervalId);
+  btnStartEl.removeAttribute('disabled');
+  btnStopEl.setAttribute('disabled', '');
+};
+
+btnStartEl.addEventListener('click', startSwitcher);
+btnStopEl.addEventListener('click', stopSwitcher);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
